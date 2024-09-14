9c65f37467e0314ba57b09c22f38cf85213ed61b
Sagnik Ghosh <sagnikg@andrew.cmu.edu>
2024-09-13 23:03:15 -0400

    Final cleanup

--- assgn1/src/main.py
+++ assgn1/src/main.py
@@ -1,8 +1,9 @@
 import numpy as np
 import skimage
 import argparse
+import matplotlib.pyplot as plt
 
-from utils import linearize_image
+from utils import linearize_image, wb_grayworld, wb_whiteworld, wb_rescale, demosaic, linear_brightening, gamma_encoding, wb_manual
 
 def parse_args():
 
@@ -13,6 +14,16 @@ def parse_args():
     parser.add_argument('--black', default=150, help='Black level')
 
     parser.add_argument('--white', default=4095, help='Saturation level')
+
+    parser.add_argument('--r_scale', default=2.394531, help='Red channel scale for white balancing')
+
+    parser.add_argument('--g_scale', default=1.0, help='Green channel scale for white balancing')
+
+    parser.add_argument('--b_scale', default=1.597656, help='Blue channel scale for white balancing')
+
+    parser.add_argument('--target_brightness', default=0.25, help='Target brightness before gamma encoding')
+
+    parser.add_argument('--quality', default=60, help='JPG quality')
 
     args = parser.parse_args()
 
@@ -33,7 +44,83 @@ if __name__=="__main__":
     im = linearize_image(im, args.black, args.white)
 
     # Identifying bayer pattern
-    print(f"Top left 2x2 square: {im[:2,:2]}")
+    x=2000
+    print(f"Top left 2x2 square: {im[x:x+2,x:x+2]}")
 
     # White balancing
-    
+    im_gw_rggb = wb_grayworld(im, 'rggb')
+    im_gw_bggr = wb_grayworld(im, 'bggr')
+    im_ww_rggb = wb_whiteworld(im, 'rggb')
+    im_ww_bggr = wb_whiteworld(im, 'bggr')
+    im_rs_rggb = wb_rescale(im, 'rggb', args.r_scale, args.g_scale, args.b_scale)
+    im_rs_bggr = wb_rescale(im, 'bggr', args.r_scale, args.g_scale, args.b_scale)
+
+    # Demosaicing
+    im_gw_rggb = demosaic(im_gw_rggb, 'rggb')
+    im_gw_bggr = demosaic(im_gw_bggr, 'bggr')
+    im_ww_rggb = demosaic(im_ww_rggb, 'rggb')
+    im_ww_bggr = demosaic(im_ww_bggr, 'bggr')
+    im_rs_rggb = demosaic(im_rs_rggb, 'rggb')
+    im_rs_bggr = demosaic(im_rs_bggr, 'bggr')
+
+    # Vizualize rggb and bggr to decide
+    skimage.io.imsave("../data/rggb.jpg", np.clip((im_ww_rggb*255).astype(np.uint8)*3, 0, 255))
+    skimage.io.imsave("../data/bggr.jpg", np.clip((im_ww_bggr*255).astype(np.uint8)*3, 0, 255))
+
+    # Color space correction
+    M_srgb2xyz = np.array([[0.4124564, 0.3575761, 0.1804375],
+                           [0.2126729, 0.7151522, 0.0721750],
+                           [0.0193339, 0.1191920, 0.9503041]])
+    M_xyz2cam = np.array([[6988,-1384,-714],
+                          [-5631,13410,2447],
+                          [-1485,2204,7318]]) / 10000
+    M_srgb2cam = M_xyz2cam @ M_srgb2xyz
+    M_srgb2cam = M_srgb2cam / np.sum(M_srgb2cam, axis=1, keepdims=True)
+
+    im_gw_rggb = (np.linalg.inv(M_srgb2cam) @ im_gw_rggb[:,:,:,None])[:,:,:,0]
+    im_ww_rggb = (np.linalg.inv(M_srgb2cam) @ im_ww_rggb[:,:,:,None])[:,:,:,0]
+    im_rs_rggb = (np.linalg.inv(M_srgb2cam) @ im_rs_rggb[:,:,:,None])[:,:,:,0]
+
+    # Brightening
+    im_gw_rggb = linear_brightening(im_gw_rggb, args.target_brightness)
+    im_ww_rggb = linear_brightening(im_ww_rggb, args.target_brightness)
+    im_rs_rggb = linear_brightening(im_rs_rggb, args.target_brightness)
+
+    # Gamma encoding
+    im_gw_rggb = gamma_encoding(im_gw_rggb)
+    im_ww_rggb = gamma_encoding(im_ww_rggb)
+    im_rs_rggb = gamma_encoding(im_rs_rggb)
+
+    # Visualize the different white-balancing methods
+    skimage.io.imsave("../data/gw.png", np.clip((im_gw_rggb*255).astype(np.uint8), 0, 255))
+    skimage.io.imsave("../data/ww.png", np.clip((im_ww_rggb*255).astype(np.uint8), 0, 255))
+    skimage.io.imsave("../data/rs.png", np.clip((im_rs_rggb*255).astype(np.uint8), 0, 255))
+
+    # Save in png format
+    skimage.io.imsave("../data/ww.png", np.clip((im_ww_rggb*255).astype(np.uint8), 0, 255))
+    # Save in jpg format
+    skimage.io.imsave(f"../data/ww_{args.quality}.jpg", np.clip((im_ww_rggb*255).astype(np.uint8), 0, 255), quality=args.quality)
+
+    # Manual white-balancing
+    pt1 = [2592, 894]
+    pt2 = [2774, 4078]
+    pt3 = [406, 4310]
+
+    im_wb_pt1 = wb_manual(im, pt1[0], pt1[1])
+    im_wb_pt2 = wb_manual(im, pt2[0], pt2[1])
+    im_wb_pt3 = wb_manual(im, pt3[0], pt3[1])
+    im_wb_pt1 = demosaic(im_wb_pt1, 'rggb')
+    im_wb_pt2 = demosaic(im_wb_pt2, 'rggb')
+    im_wb_pt3 = demosaic(im_wb_pt3, 'rggb')
+    im_wb_pt1 = (np.linalg.inv(M_srgb2cam) @ im_wb_pt1[:,:,:,None])[:,:,:,0]
+    im_wb_pt2 = (np.linalg.inv(M_srgb2cam) @ im_wb_pt2[:,:,:,None])[:,:,:,0]
+    im_wb_pt3 = (np.linalg.inv(M_srgb2cam) @ im_wb_pt3[:,:,:,None])[:,:,:,0]
+    im_wb_pt1 = linear_brightening(im_wb_pt1, args.target_brightness)
+    im_wb_pt2 = linear_brightening(im_wb_pt2, args.target_brightness)
+    im_wb_pt3 = linear_brightening(im_wb_pt3, args.target_brightness)
+    im_wb_pt1 = gamma_encoding(im_wb_pt1)
+    im_wb_pt2 = gamma_encoding(im_wb_pt2)
+    im_wb_pt3 = gamma_encoding(im_wb_pt3)
+    skimage.io.imsave("../data/wb_pt1.png", np.clip((im_wb_pt1*255).astype(np.uint8), 0, 255))
+    skimage.io.imsave("../data/wb_pt2.png", np.clip((im_wb_pt2*255).astype(np.uint8), 0, 255))
+    skimage.io.imsave("../data/wb_pt3.png", np.clip((im_wb_pt3*255).astype(np.uint8), 0, 255))
--- assgn1/src/utils.py
+++ assgn1/src/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.interpolate import RegularGridInterpolator
+from skimage.color import rgb2gray
 
 def linearize_image(im, black, white):
     """Linearize image given the black and white vals."""
@@ -44,6 +46,34 @@ def wb_grayworld(im, pattern):
 
         im = im / np.max(im)
 
+    elif pattern=='rggb':
+        # Compute means of each channel
+        red_mean = np.mean(im[::2,::2]) # Red
+        green_mean = (np.mean(im[::2,1::2]) + np.mean(im[1::2,::2])) / 2 # Green
+        blue_mean = np.mean(im[1::2,1::2]) # Blue
+
+        # Enforce gray world assumption
+        im[::2,::2] = im[::2,::2] / red_mean
+        im[::2,1::2] = im[::2,1::2] / green_mean
+        im[1::2,::2] = im[1::2,::2] / green_mean
+        im[1::2,1::2] = im[1::2,1::2] / blue_mean
+
+        im = im / np.max(im)
+
+    elif pattern=='bggr':
+        # Compute means of each channel
+        blue_mean = np.mean(im[::2,::2]) # Blue
+        green_mean = (np.mean(im[::2,1::2]) + np.mean(im[1::2,::2])) / 2 # Green
+        red_mean = np.mean(im[1::2,1::2]) # Red
+
+        # Enforce gray world assumption
+        im[::2,::2] = im[::2,::2] / blue_mean
+        im[::2,1::2] = im[::2,1::2] / green_mean
+        im[1::2,::2] = im[1::2,::2] / green_mean
+        im[1::2,1::2] = im[1::2,1::2] / red_mean
+
+        im = im / np.max(im)
+
     else:
         raise NotImplementedError
     
@@ -58,7 +88,7 @@ def wb_whiteworld(im, pattern):
     """
     # Find brightest point in scene
     im_brightness = im[::2,1::2] / 3 + im[::2,::2] / 6 + im[1::2,1::2] / 6 + im[1::2,::2] / 3
-    tgt_idx = 2*np.argmax(im_brightness)
+    tgt_idx = 2*np.unravel_index(np.argmax(im_brightness), im_brightness.shape)
     tgt_grid = im[tgt_idx[0]:tgt_idx[0]+2,tgt_idx[1]:tgt_idx[1]+2]
 
     if pattern=='grbg':
@@ -82,6 +112,32 @@ def wb_whiteworld(im, pattern):
         im[::2,::2] = im[::2,::2] / green_val
         im[1::2,1::2] = im[1::2,1::2] / green_val
         im[1::2,::2] = im[1::2,::2] / red_val
+
+        im = im / np.max(im)
+
+    elif pattern=='rggb':
+        red_val = tgt_grid[0,0]
+        green_val = (tgt_grid[0,1] + tgt_grid[1,0]) / 2
+        blue_val = tgt_grid[1,1]
+
+        # Enforce white world assumption
+        im[::2,::2] = im[::2,::2] / red_val
+        im[::2,1::2] = im[::2,1::2] / green_val
+        im[1::2,::2] = im[1::2,::2] / green_val
+        im[1::2,1::2] = im[1::2,1::2] / blue_val
+
+        im = im / np.max(im)
+
+    elif pattern=='bggr':
+        red_val = tgt_grid[1,1]
+        green_val = (tgt_grid[0,1] + tgt_grid[1,0]) / 2
+        blue_val = tgt_grid[0,0]
+
+        # Enforce white world assumption
+        im[::2,::2] = im[::2,::2] / blue_val
+        im[::2,1::2] = im[::2,1::2] / green_val
+        im[1::2,::2] = im[1::2,::2] / green_val
+        im[1::2,1::2] = im[1::2,1::2] / red_val
 
         im = im / np.max(im)
 
@@ -116,8 +172,187 @@ def wb_rescale(im, pattern, r_scale, g_scale, b_scale):
 
         im = im / np.max(im)
 
+    elif pattern=='rggb':
+        # Rescale
+        im[::2,::2] = im[::2,::2] * r_scale
+        im[::2,1::2] = im[::2,1::2] * g_scale
+        im[1::2,::2] = im[1::2,::2] * g_scale
+        im[1::2,1::2] = im[1::2,1::2] * b_scale
+
+        im = im / np.max(im)
+
+    elif pattern=='bggr':
+        # Rescale
+        im[::2,::2] = im[::2,::2] * b_scale
+        im[::2,1::2] = im[::2,1::2] * g_scale
+        im[1::2,::2] = im[1::2,::2] * g_scale
+        im[::2,::2] = im[::2,::2] * r_scale
+
+        im = im / np.max(im)
+
     else:
         raise NotImplementedError
     
     return im
-        
+
+def demosaic(im, pattern):
+    """
+    Demosaic given RAW image based on the pattern.
+
+    Note that this is only implemented for grbg and gbrg as
+    those are the only 2 possibilities.
+    """
+
+    # Define meshgrid
+    X = np.arange(im.shape[1])
+    Y = np.arange(im.shape[0])
+    X, Y = np.meshgrid(X, Y, indexing='xy')
+
+    if pattern=='grbg':
+        # Interpolate red channel
+        x = np.arange(1,im.shape[1],2)
+        y = np.arange(0,im.shape[0],2)
+        interp_red = RegularGridInterpolator((y, x), im[::2,1::2], bounds_error=False, fill_value=None)
+        im_red = interp_red((Y,X))
+
+        # Interpolate green channel
+        x = np.arange(0,im.shape[1],2)
+        y = np.arange(0,im.shape[0],2)
+        interp_green1 = RegularGridInterpolator((y, x), im[::2,::2], bounds_error=False, fill_value=None)
+        im_green1 = interp_green1((Y,X))
+
+        x = np.arange(1,im.shape[1],2)
+        y = np.arange(1,im.shape[0],2)
+        interp_green2 = RegularGridInterpolator((y, x), im[1::2,1::2], bounds_error=False, fill_value=None)
+        im_green2 = interp_green2((Y,X))
+
+        im_green = (im_green1 + im_green2) / 2
+
+        # Interpolate blue channel
+        x = np.arange(0,im.shape[1],2)
+        y = np.arange(1,im.shape[0],2)
+        interp_blue = RegularGridInterpolator((y, x), im[1::2,::2], bounds_error=False, fill_value=None)
+        im_blue = interp_blue((Y,X))
+
+    elif pattern=='gbrg':
+        # Interpolate red channel
+        x = np.arange(0,im.shape[1],2)
+        y = np.arange(1,im.shape[0],2)
+        interp_red = RegularGridInterpolator((y, x), im[1::2,::2], bounds_error=False, fill_value=None)
+        im_red = interp_red((Y,X))
+
+        # Interpolate green channel
+        x = np.arange(0,im.shape[1],2)
+        y = np.arange(0,im.shape[0],2)
+        interp_green1 = RegularGridInterpolator((y, x), im[::2,::2], bounds_error=False, fill_value=None)
+        im_green1 = interp_green1((Y,X))
+
+        x = np.arange(1,im.shape[1],2)
+        y = np.arange(1,im.shape[0],2)
+        interp_green2 = RegularGridInterpolator((y, x), im[1::2,1::2], bounds_error=False, fill_value=None)
+        im_green2 = interp_green2((Y,X))
+
+        im_green = (im_green1 + im_green2) / 2
+
+        # Interpolate blue channel
+        x = np.arange(1,im.shape[1],2)
+        y = np.arange(0,im.shape[0],2)
+        interp_blue = RegularGridInterpolator((y, x), im[::2,1::2], bounds_error=False, fill_value=None)
+        im_blue = interp_blue((Y,X))
+
+    elif pattern=='rggb':
+        # Interpolate red channel
+        x = np.arange(0,im.shape[1],2)
+        y = np.arange(0,im.shape[0],2)
+        interp_red = RegularGridInterpolator((y, x), im[::2,::2], bounds_error=False, fill_value=None)
+        im_red = interp_red((Y,X))
+
+        # Interpolate green channel
+        x = np.arange(0,im.shape[1],2)
+        y = np.arange(1,im.shape[0],2)
+        interp_green1 = RegularGridInterpolator((y, x), im[::2,1::2], bounds_error=False, fill_value=None)
+        im_green1 = interp_green1((Y,X))
+
+        x = np.arange(1,im.shape[1],2)
+        y = np.arange(0,im.shape[0],2)
+        interp_green2 = RegularGridInterpolator((y, x), im[1::2,::2], bounds_error=False, fill_value=None)
+        im_green2 = interp_green2((Y,X))
+
+        im_green = (im_green1 + im_green2) / 2
+
+        # Interpolate blue channel
+        x = np.arange(1,im.shape[1],2)
+        y = np.arange(1,im.shape[0],2)
+        interp_blue = RegularGridInterpolator((y, x), im[1::2,1::2], bounds_error=False, fill_value=None)
+        im_blue = interp_blue((Y,X))
+
+    elif pattern=='bggr':
+        # Interpolate red channel
+        x = np.arange(1,im.shape[1],2)
+        y = np.arange(1,im.shape[0],2)
+        interp_red = RegularGridInterpolator((y, x), im[1::2,1::2], bounds_error=False, fill_value=None)
+        im_red = interp_red((Y,X))
+
+        # Interpolate green channel
+        x = np.arange(0,im.shape[1],2)
+        y = np.arange(1,im.shape[0],2)
+        interp_green1 = RegularGridInterpolator((y, x), im[::2,1::2], bounds_error=False, fill_value=None)
+        im_green1 = interp_green1((Y,X))
+
+        x = np.arange(1,im.shape[1],2)
+        y = np.arange(0,im.shape[0],2)
+        interp_green2 = RegularGridInterpolator((y, x), im[1::2,::2], bounds_error=False, fill_value=None)
+        im_green2 = interp_green2((Y,X))
+
+        im_green = (im_green1 + im_green2) / 2
+
+        # Interpolate blue channel
+        x = np.arange(0,im.shape[1],2)
+        y = np.arange(0,im.shape[0],2)
+        interp_blue = RegularGridInterpolator((y, x), im[::2,::2], bounds_error=False, fill_value=None)
+        im_blue = interp_blue((Y,X))
+
+    else:
+        raise NotImplementedError
+    
+    im_rgb = np.stack([im_red, im_green, im_blue], axis=-1)
+
+    return im_rgb
+
+def linear_brightening(im, target_brightness):
+    """
+    This implements the brightening step before gamma encoding.
+    """
+
+    im = im * target_brightness / np.mean(rgb2gray(im))
+    im = np.clip(im, 0, 1)
+
+    return im
+
+def gamma_encoding(im):
+    """
+    This implements the gamma-encoding step.
+    """
+
+    thresh_mask = (im <= 0.0031308)
+
+    im[thresh_mask] = 12.92 * im[thresh_mask]
+
+    im[np.logical_not(thresh_mask)] = 1.055 * im[np.logical_not(thresh_mask)] ** (1/2.4) - 0.055
+
+    return im
+
+def wb_manual(im, pt_y, pt_x):
+
+    r_val = im[pt_y, pt_x]
+    g_val = (im[pt_y+1, pt_x] + im[pt_y, pt_x+1]) / 2
+    b_val = im[pt_y+1, pt_x+1]
+
+    im[::2,::2] = im[::2,::2] / r_val
+    im[::2,1::2] = im[::2,1::2] / g_val
+    im[1::2,::2] = im[1::2,::2] / g_val
+    im[1::2,1::2] = im[1::2,1::2] / b_val
+
+    im = im / np.max(im)
+
+    return im
