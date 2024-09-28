0c5a45d067eebee64bbdd2773b41cff10f562e36
Sagnik Ghosh <144840125+sagnikgh1@users.noreply.github.com>
2024-09-27 22:44:38 -0400

    Update noise_calib.py

--- assgn2/src/noise_calib.py
+++ assgn2/src/noise_calib.py
@@ -51,22 +51,22 @@ if __name__=="__main__":
     plt.hist(ramps[:,1600,1800])
     plt.show()
 
-    # mean_map = np.mean(ramps, axis=0)
-    # var_map = (1/(ramps.shape[0]-1)) * np.sum((ramps - mean_map[None,:,:])**2, axis=0)
-    # rounded_mean_map = np.round(mean_map).astype(int)
-    # rounded_mean_map = rounded_mean_map
-    # mean_unique = np.unique(rounded_mean_map)
-    # avg_vars = []
-    # for mean in mean_unique:
-    #     avg_var = np.mean(var_map[rounded_mean_map==mean])
-    #     avg_vars.append(avg_var)
-    # avg_vars = np.array(avg_vars)
+    mean_map = np.mean(ramps, axis=0)
+    var_map = (1/(ramps.shape[0]-1)) * np.sum((ramps - mean_map[None,:,:])**2, axis=0)
+    rounded_mean_map = np.round(mean_map).astype(int)
+    rounded_mean_map = rounded_mean_map
+    mean_unique = np.unique(rounded_mean_map)
+    avg_vars = []
+    for mean in mean_unique:
+        avg_var = np.mean(var_map[rounded_mean_map==mean])
+        avg_vars.append(avg_var)
+    avg_vars = np.array(avg_vars)
 
-    # # Fit line
-    # p = np.polyfit(mean_unique[:2000], avg_vars[:2000], deg=1)
-    # print(p)
+    # Fit line
+    p = np.polyfit(mean_unique[:2000], avg_vars[:2000], deg=1)
+    print(p)
 
-    # plt.plot(mean_unique, avg_vars)
-    # plt.plot(mean_unique, p[0]*mean_unique + p[1])
-    # plt.show()
+    plt.plot(mean_unique, avg_vars)
+    plt.plot(mean_unique, p[0]*mean_unique + p[1])
+    plt.show()
 
